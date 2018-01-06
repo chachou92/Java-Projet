49e8b464ca8210230cead20d3248a768d46905b7
Charlotte <Volleyball1a>
2018-01-06 17:35:21 +0100

    Creation des classes Cours et Etudiant

--- src/com/company/Etudiant.java
+++ src/com/company/Etudiant.java
@@ -1,6 +1,8 @@
 package com.company;
 
+import java.io.*;
 import java.util.ArrayList;
+
 
 public class Etudiant {
     private int id;
@@ -11,7 +13,28 @@ public class Etudiant {
         listeCours = liste;
     }
     public Etudiant (){
-        id = "";
+        id = 0;
         listeCours = new ArrayList<String>();
     }
+
+    //Méthode getId à faire et get listeCours
+
+
+
+    public void LireFichierEtu(File f){
+        try {
+            //Ouverture du fichier étudiant
+            File f = new File (System.getProperty("user.dir")+"/etu.csv");
+            InputStreamReader reader = new InputStreamReader(f);
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            String line = bufferedReader.readLine();
+            while (line != null){
+                line  = bufferedReader.readLine();
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
